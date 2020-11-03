6bcdcdbb014f35c037ba3aebc23fdb5f10c18744
mshamburger1 <michael.shamburger@gmail.com>
2020-11-03 12:26:16 -0600

    Begin again

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <h1>HI</h1>
+        <Footer />
+      </>
+    );
+  }
+}
